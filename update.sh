2b1b6f49b55c26c8cf94738ec6bc1ad67a93ac5b
Kushal Hada <kus@tilde.team>
2021-04-23 11:05:40 -0600

    update angular
    from a shell script

--- update.sh
+++ update.sh
@@ -2,10 +2,13 @@
 
 cd /home/kushal/src/angular/angulartensecondlook;
 time yarn run ng update;
+time yarn run ng update @angular/core @angular/cli @angular/cdk @angular/material @angular/localize rxjs;
+time git add .;
+time git commit -m "update angular" -m "from a shell script"
 time yarn run ng build --configuration=production;
 time yarn run ng test;
 du -sh dist/angulartensecondlook/;
-time rsync -av /home/kushal/angular/AngularTenSecondLook/dist/angulartensecondlook/ /home/kushal/src/angular/angulartengithubio;
+time rsync -av /home/kushal/angular/angulartensecondlook/dist/angulartensecondlook/ /home/kushal/src/angular/angulartengithubio;
 du -sh /home/kushal/src/angular/angulartengithubio;
 cd /home/kushal/src/angular/angulartengithubio/;
 git add .;
