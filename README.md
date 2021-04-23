ecb15e3b7868f5be9001dc3817af8057dc69f248
Kushal Hada <kus@tilde.team>
2021-04-23 10:51:03 -0600

    update readme and update script

--- README.md
+++ README.md
@@ -7,12 +7,12 @@ https://github.com/angularten/angularten.github.io
 ```bash
 #!/bin/bash
 
-cd /home/kushal/src/html/AngularTenSecondLook;
+cd /home/kushal/src/html/angulartensecondlook;
 time yarn run ng update;
 time yarn run ng build --configuration=production;
 time yarn run ng test;
-du -sh dist/AngularTenSecondLook/;
-time rsync -av /home/kushal/src/html/AngularTenSecondLook/dist/AngularTenSecondLook/ /home/kushal/src/html/angulartengithubio;
+du -sh dist/angulartensecondlook/;
+time rsync -av /home/kushal/angular/html/AngularTenSecondLook/dist/AngularTenSecondLook/ /home/kushal/src/angular/angulartengithubio;
 du -sh /home/kushal/src/html/angulartengithubio;
 cd /home/kushal/src/html/angulartengithubio/;
 git add .;
--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd /home/kushal/src/html/AngularTenSecondLook;
+cd /home/kushal/src/angular/angulartensecondlook;
 time yarn run ng update;
 time yarn run ng build --configuration=production;
 time yarn run ng test;
-du -sh dist/AngularTenSecondLook/;
-time rsync -av /home/kushal/src/html/AngularTenSecondLook/dist/AngularTenSecondLook/ /home/kushal/src/html/angulartengithubio;
-du -sh /home/kushal/src/html/angulartengithubio;
-cd /home/kushal/src/html/angulartengithubio/;
+du -sh dist/angulartensecondlook/;
+time rsync -av /home/kushal/angular/AngularTenSecondLook/dist/angulartensecondlook/ /home/kushal/src/angular/angulartengithubio;
+du -sh /home/kushal/src/angular/angulartengithubio;
+cd /home/kushal/src/angular/angulartengithubio/;
 git add .;
 git commit -m "build and deploy";
 time git push origin master;
