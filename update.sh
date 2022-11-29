9fc22542f51ae67d5b3b4979a0dab576f940fbb5
Kushal Hada <kus@tilde.team>
2022-11-29 10:11:04 -0500

    update update.sh from angular twelve project

--- update.sh
+++ update.sh
@@ -1,24 +1,99 @@
 #!/bin/bash
 
-cd /home/kushal/src/angular/angulartensecondlook;
-free -h;
+cd ~/src/angular/AngularTenSecondLook/;
+ping -A -D -c 20 -v 8.8.8.8 > ping.txt;
+date > README.md;
+git add .;
+git commit -m "add timestamp";
+
+echo '' >> README.md 2>&1;
+echo '# Angular Twelve' >> README.md 2>&1;
+echo '' >> README.md 2>&1;
+
+echo "\`\`\`bash" >> README.md 2>&1;
+ping -A -D -c 20 -v 8.8.8.8 >> ping.txt 2>&1;
+git add .;
+git commit -m "begin add system status";
+echo "System Memory" >> README.md 2>&1;
+free -h >> README.md 2>&1;
+echo "System Storage" >> README.md 2>&1;
+du -sh .>> README.md 2>&1;
+git add .;
+git commit -m "add system status";
+echo "\`\`\`" >> README.md 2>&1;
+git add .;
+git commit -m "end add system status";
+
+
+echo "\`\`\`bash" >> README.md 2>&1;
+ping -A -D -c 20 -v 8.8.8.8 >> ping.txt 2>&1;
+git add .;
+git commit -m "begin update node";
+[[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh;  # This loads NVM
 source ~/.nvm/nvm.sh;
 time nvm install --lts;
 time nvm use --lts;
-time npm install --global @angular/cli yarn;
-export NODE_OPTIONS="--max-old-space-size=6144";
-time yarn run ng update;
-time yarn run ng update @angular/core @angular/cli @angular/cdk @angular/material;
-time yarn version --patch;
-time git add .;
-time git commit -m "update angular" -m "from a shell script";
-time git push origin master;
-time yarn run ng build --configuration=production;
-time yarn run ng test;
-du -sh dist/angulartensecondlook/;
-time rsync -av /home/kushal/src/angular/angulartensecondlook/dist/AngularTenSecondLook/ /home/kushal/src/angular/angulartengithubio;
-du -sh /home/kushal/src/angular/angulartengithubio;
-cd /home/kushal/src/angular/angulartengithubio/;
+export NODE_OPTIONS="--max-old-space-size=8000"; time npm install --global @angular/cli yarn;
+export NODE_OPTIONS="--max-old-space-size=8000"; time yarn run ng version >> README.md 2>&1;
+time yarn >> README.md 2>&1;
+echo "\`\`\`" >> README.md 2>&1;
+ping -A -D -c 20 -v 8.8.8.8 >> ping.txt 2>&1;
+git add .;
+git commit -m "end update node";
+
+export NODE_OPTIONS="--max-old-space-size=8000";
+
+git add .;
+git commit -m "begin prepare to update angular";
+export NODE_OPTIONS="--max-old-space-size=8000"; time yarn run ng update @angular/core @angular/cli @angular/material @angular/localize;
+git add .;
+git commit -m "end prepare to update angular";
+
+echo "\`\`\`bash" >> README.md 2>&1;
+time npx browserslist --update-db >> README.md 2>&1;
+echo "\`\`\`" >> README.md 2>&1;
+
+echo "\`\`\`bash" >> README.md 2>&1;
+ping -A -D -c 20 -v 8.8.8.8 >> ping.txt 2>&1;
+git add .;
+git commit -m "begin prepare to build angular";
+export NODE_OPTIONS="--max-old-space-size=8000"; time yarn run ng build --configuration production >> README.md 2>&1;
+echo "\`\`\`" >> README.md 2>&1;
+git add .;
+git commit -m "end prepare to build angular";
+
+echo "\`\`\`bash" > locallog/fedoratest.md;
+ping -A -D -c 20 -v 8.8.8.8 >> ping.txt 2>&1;
+git add .;
+git commit -m "begin prepare to unit test angular";
+export NODE_OPTIONS="--max-old-space-size=8000"; time yarn run ng test >> locallog/fedoratest.md;
+echo "\`\`\`" >> locallog/fedoratest.md;
+ping -A -D -c 20 -v 8.8.8.8 >> ping.txt 2>&1;
+git add .;
+git commit -m "end prepare to unit test angular";
+
+date >> README.md 2>&1;
+ping -D -c 20 -v 8.8.8.8 >> ping.txt 2>&1;
+
+date >> README.md;
+time yarn version --patch >> README.md;
+git add .;
+git commit -m "add timestamp";
+git pull --rebase origin master --strategy-option=ours;
+git add .;
+git commit -m "merge from remote";
+git push origin master;
+
+
+du -sh dist/AngularTenSecondLook/;
+time rsync -av /home/kushal/src/angular/AngularTenSecondLook/dist/AngularTenSecondLook/ /home/kushal/src/angular/angulrtwelvegithubio;
+du -sh /home/kushal/src/angular/angulrtwelvegithubio;
+cd /home/kushal/src/angular/angulrtwelvegithubio/;
 git add .;
 git commit -m "build and deploy";
+COMMIT_ID=$(git rev-parse --verify HEAD)
+curl https://sentry.io/api/hooks/release/builtin/5768670/b50116ba5ad9986947365aded6efb721e66310c9557954a43fe58fe0c27df2a4/ \
+  -X POST \
+  -H 'Content-Type: application/json' \
+  -d '{"version": "'"$COMMIT_ID"'"}'
 time git push origin master;
